d232e718d731ca80a8ddfe9d9a6002eb4fa0b840
ellison3459 <92761717+ellison3459@users.noreply.github.com>
2021-11-03 19:06:23 -0300

    final

--- vendas.js
+++ vendas.js
@@ -1,18 +1,18 @@
 var frmModelo;
-var frmValor= parseInt();
+var frmValor = parseInt();
 var divMostrar;
 
 //Botao cadastrar add evento
-btnSimular.addEventListener('click', function(){
-    frmModelo = modelo.value 
-    frmValor = valor.value
-    divMostrar = document.getElementById('calculo')
-   
-    divMostrar.innerHTML = `
+btnSimular.addEventListener('click', function () {
+  frmModelo = modelo.value
+  frmValor = valor.value
+  divMostrar = document.getElementById('calculo')
+
+  divMostrar.innerHTML = `
         <div>Modelo: <strong>${frmModelo}</strong></div>
-        <div>Valor: <strong>${frmValor}</strong></div>
-        <div>Valor da entrada: <strong>${frmValor/2}</strong></div>
-        <div>60 Parcelas no valor de R$: <strong>${((((frmValor/2)/60)*0.70)+((frmValor/2)/60)).toFixed(2)}</strong></div>
+        <div>Valor R$: <strong>${frmValor}</strong></div>
+        <div>Valor da entrada R$:<strong>${frmValor / 2}</strong></div>
+        <div>60 Parcelas no valor de R$: <strong>${((((frmValor / 2) / 60) * 0.70) + ((frmValor / 2) / 60)).toFixed(2)}</strong></div>
   `
-    
+
 })
