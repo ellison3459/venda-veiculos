f2fddab0a2bc13ace6507d1fbd88f96eadb6062f
ellison3459 <ellison3459@gmail.com>
2021-11-03 16:02:54 -0300

    2 commit

--- vendas.js
+++ vendas.js
@@ -1,15 +1,26 @@
 function calcular(){
 
-    var vValor = document.getElementById("valor").value;
+    var vValor = document.getElementById("valor").
+    value;
+    
+    
     var cEntrada = vValor*50;
     var pEntrada = cEntrada/100;
     var cParcela = vValor-pEntrada;
     var pParcela = cParcela/60;
+    var cjuros = pParcela*70;
+    var pjuros = cjuros/100+pParcela;
     
-
+    
     alert("Valor da entrada:"+pEntrada)
-    alert("+ 60 de R$:"+pParcela)
+    alert("+ 60 de R$:"+pjuros)
 
 
     let result = document.getElementById("result")
 }
+
+
+/* function mostrarTexto () {
+ visualizar.addEventListener('click', function())
+}
+ */
